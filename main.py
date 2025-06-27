7aec0bf149d1553495384487f6740533d1cc0279
Daniel <zifeng.xu@outlook.com>
2025-06-26 22:19:11 -0400

    build new version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkinter import filedialog, messagebox, scrolledtext
 import json
 
 APP_NAME = "Project Folder Manager"
-VERSION = "2.0.1"
+VERSION = "2.1.0"
 APP_TITLE = f"{APP_NAME} v{VERSION}"
 
 # Use a persistent structure file in the exe/script directory
--- main.spec
+++ main.spec
@@ -22,7 +22,7 @@ exe = EXE(
     a.binaries,
     a.datas,
     [],
-    name='Project Folder Manager v2.0.1',
+    name='Project Folder Manager v2.1.0',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
